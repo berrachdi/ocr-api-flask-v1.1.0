69d74ca22c8a11248019ed26d3e8285afe08baf6
Berrachdi Mohamed <mohamed_berrachdi@um5.ac.ma>
2022-06-28 19:46:43 +0200

    last commit

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, request, redirect, jsonify
+from flask import Flask, request, redirect, jsonify, send_from_directory
 from werkzeug.utils import secure_filename
 import os
 import urllib.request
 from pyresparser import ResumeParser
 import nltk
 import spacy
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+from flask_swagger_ui import get_swaggerui_blueprint
+
 
 
 spacy.load('en_core_web_sm')
@@ -12,13 +16,62 @@ spacy.load('en_core_web_sm')
 UPLOAD_FOLDER = './files/'
 
 app = Flask(__name__)
+
+# Config MySQL database
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@127.0.0.1:3306/imagine_ocr_db"
+
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# swagger config
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.yml'
+SWAGGER_BLUEPRINT = get_swaggerui_blueprint(
+	SWAGGER_URL,
+	API_URL,
+	config={
+		'app_name':'OCR Engine service'
+	}
+
+)
+
+app.register_blueprint(SWAGGER_BLUEPRINT,url_prefix = SWAGGER_URL)
 
 ALLOWED_EXTENSIONS = set(['pdf'])
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+# Initialize the database
+db = SQLAlchemy(app)
+# Create a model
+class Users(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer,primary_key = True)
+    email = db.Column(db.String(200))
+    skills =  db.Column(db.String(1000))
+    expertise = db.Column(db.String(1000))
+    formation =  db.Column(db.String(1000))
+    phone =  db.Column(db.String(20))
+    fullName =  db.Column(db.String(100))
+    address =   db.Column(db.String(200))
+    def __repr__(self):
+        return '<User {}>'.format(self.email)
+
+# data boucle
+def exctract_data(items):
+    if items is None:
+        return ""
+    else:
+        return ' '.join([str(item) for item in items])
+
+#swagger endpoint
+
+
+
+
+
 
 @app.route('/file-upload', methods=['POST'])
 def upload_file():
@@ -40,8 +93,25 @@ def upload_file():
 		#OCR Engine 
 		data = ResumeParser(app.config['UPLOAD_FOLDER']+filename).get_extracted_data()
 
-		resp = jsonify(data)
+		
 
+		# Save data in MySQL Database
+		user = Users(
+
+            email=data['email'],
+            fullName = data['name'],
+            address = "",
+            skills = exctract_data(data['skills']),
+            expertise = exctract_data(data['experience']),
+            formation = exctract_data(data['degree']),
+            phone = data['mobile_number']
+
+        )
+		db.session.add(user)
+		db.session.commit()
+		db.session.close()
+        
+		resp = jsonify(data)
 		resp.status_code = 201
 		return resp
 
